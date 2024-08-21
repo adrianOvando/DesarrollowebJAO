9ef15f7d461f6854d71c58b6d24d890797870971
Adrian Ovando <adrianovando138@gmail.com>
2024-08-21 17:11:24 -0400

    .

---
+++ Ejercicios en clases/tema 3/clase 2/cuadrado.php
@@ -0,0 +1,15 @@
+<?php 
+    $cadena="sistemas";
+    $longitud=strlen($cadena);
+    echo strtoupper($cadena), "<br>";
+    for($i=1;$i<$longitud-1;$i++){
+        echo substr(strtoupper($cadena), $i, 1);
+        for($j=1;$j<=$longitud;$i++){
+            echo"&nbsp;";
+        }
+        echo substr($cadena, $longitud-$i-1, 1)."<br>";
+    }
+    for($k=1; $k<$longitud;$k++){
+        echo substr(strtoupper($cadena), $i, 1);
+    }
+?>
