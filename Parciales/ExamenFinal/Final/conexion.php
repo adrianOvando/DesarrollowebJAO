704d3cd1be627432c866cde553af77e09cab70ce
Adrian Ovando <adrianovando138@gmail.com>
2024-12-02 17:37:57 -0400

    Final 2/12/2024

--- Parciales/ExamenFinal/Final/conexion.php
+++ Parciales/ExamenFinal/Final/conexion.php
@@ -1,5 +1,5 @@
 <?php
-$con = new mysqli("localhost:3307", "root", "", "bd_biblioteca_central");
+$con = new mysqli("localhost", "root", "", "bd_biblioteca_central");
 if($con->connect_error)
 {
     die("Error: " . $con->connect_error);
